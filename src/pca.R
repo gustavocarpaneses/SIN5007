4b111b758e20b26054cd2190210d99fe9466a6af
Adriano <adriano.each@gmail.com>
2017-11-15 17:37:49 -0200

    Tiranso Apenas sujeira do arquivo do comiit anterior

--- src/pca.R
+++ src/pca.R
@@ -1,12 +1,10 @@
 #carregando arquivo
-<<<<<<< HEAD
+
 setwd('C:\\Users\\AdrianoDev\\Documents\\Mestrado\\Reconhecimento Padrões\\cancer\\SIN5007')
 #myData <- read.csv(file="../data/breast-cancer-wisconsin.data", header=FALSE, sep=",")
 
 myData <- read.csv(file="data/breast-cancer-wisconsin-with-class-names.data", header=FALSE, sep=",")
-=======
-myData <- read.csv(file="C:/temp/sin5007/data/breast-cancer-wisconsin.data", header=FALSE, sep=",")
->>>>>>> 155460dc2bb82cdea8ae248be3a03df2129a2b7b
+
 
 #retira a coluna que tem o identificador e a classificação
 myData.withoutIdentifierAndClassification <- myData[, 2:10]
